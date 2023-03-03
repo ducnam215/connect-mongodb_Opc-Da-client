9e7d2a03695531ae278f6e2a1ac390cb6e73a2e4
Pham Duc Nam <Pham Duc Nam@DESKTOP-7HC22TH>
2023-03-03 09:48:17 +0700

    Add project files.

---
+++ connect-mongodb/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace connect_mongodb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtWriteVal = new System.Windows.Forms.TextBox();
+            this.txtReadVal = new System.Windows.Forms.TextBox();
+            this.Label5 = new System.Windows.Forms.Label();
+            this.Label4 = new System.Windows.Forms.Label();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtWriteVal);
+            this.groupBox1.Controls.Add(this.txtReadVal);
+            this.groupBox1.Controls.Add(this.Label5);
+            this.groupBox1.Controls.Add(this.Label4);
+            this.groupBox1.Controls.Add(this.btnDisconnect);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.txtServer);
+            this.groupBox1.Controls.Add(this.txtServerName);
+            this.groupBox1.Controls.Add(this.Label6);
+            this.groupBox1.Controls.Add(this.Label2);
+            this.groupBox1.Controls.Add(this.Label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(425, 434);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connect";
+            // 
+            // txtWriteVal
+            // 
+            this.txtWriteVal.Location = new System.Drawing.Point(163, 326);
+            this.txtWriteVal.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWriteVal.Name = "txtWriteVal";
+            this.txtWriteVal.Size = new System.Drawing.Size(127, 22);
+            this.txtWriteVal.TabIndex = 49;
+            // 
+            // txtReadVal
+            // 
+            this.txtReadVal.Location = new System.Drawing.Point(163, 259);
+            this.txtReadVal.Margin = new System.Windows.Forms.Padding(4);
+            this.txtReadVal.Name = "txtReadVal";
+            this.txtReadVal.Size = new System.Drawing.Size(127, 22);
+            this.txtReadVal.TabIndex = 50;
+            // 
+            // Label5
+            // 
+            this.Label5.AutoSize = true;
+            this.Label5.Location = new System.Drawing.Point(55, 332);
+            this.Label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label5.Name = "Label5";
+            this.Label5.Size = new System.Drawing.Size(35, 16);
+            this.Label5.TabIndex = 48;
+            this.Label5.Text = "(Int2)";
+            // 
+            // Label4
+            // 
+            this.Label4.AutoSize = true;
+            this.Label4.Location = new System.Drawing.Point(54, 262);
+            this.Label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label4.Name = "Label4";
+            this.Label4.Size = new System.Drawing.Size(35, 16);
+            this.Label4.TabIndex = 47;
+            this.Label4.Text = "(Int1)";
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(206, 144);
+            this.btnDisconnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(148, 43);
+            this.btnDisconnect.TabIndex = 45;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnConnect.Location = new System.Drawing.Point(58, 144);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(148, 43);
+            this.btnConnect.TabIndex = 44;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(146, 25);
+            this.txtServer.Margin = new System.Windows.Forms.Padding(4);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(207, 22);
+            this.txtServer.TabIndex = 46;
+            this.txtServer.Text = "127.0.0.1";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(146, 84);
+            this.txtServerName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(207, 22);
+            this.txtServerName.TabIndex = 40;
+            this.txtServerName.Text = "Kepware.KEPServerEX.V6";
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(142, 112);
+            this.Label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(126, 16);
+            this.Label6.TabIndex = 42;
+            this.Label6.Text = "(OPC Server Name)";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(142, 53);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(163, 16);
+            this.Label2.TabIndex = 43;
+            this.Label2.Text = "(Host name or IP address)";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(54, 28);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(78, 16);
+            this.Label1.TabIndex = 41;
+            this.Label1.Text = "OPC Server";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(443, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(641, 430);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "MongoDB";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(629, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 458);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Connect_MongoDB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        internal System.Windows.Forms.TextBox txtWriteVal;
+        internal System.Windows.Forms.TextBox txtReadVal;
+        internal System.Windows.Forms.Label Label5;
+        internal System.Windows.Forms.Label Label4;
+        internal System.Windows.Forms.Button btnDisconnect;
+        internal System.Windows.Forms.Button btnConnect;
+        internal System.Windows.Forms.TextBox txtServer;
+        internal System.Windows.Forms.TextBox txtServerName;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ connect-mongodb/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver.Core;
+using MongoDB.Bson.Serialization.Attributes;
+using OPCAutomation;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace connect_mongodb
+{
+    public partial class Form1 : Form
+    {
+       
+      
+        public static IMongoClient client = new MongoClient("mongodb://127.0.0.1");
+
+        public static IMongoDatabase db = client.GetDatabase("mydatabase5");
+
+        public static IMongoCollection<Resident> collection = db.GetCollection<Resident>("mycol55");
+
+
+        public OPCServer MyOPCServer = new OPCServer();
+        public OPCGroup My_OPCGroup = null;
+        OPCItem MyOPCItem;
+        int[] MyServerHandles = new int[2];
+        object[] MyValues = new object[2];
+        public Array MyErrors;
+
+        int ItemCount;
+        Array OPCItemIDs = Array.CreateInstance(typeof(string), 10);
+        Array ItemServerHandles = Array.CreateInstance(typeof(Int32), 10);
+        Array ItemServerErrors = Array.CreateInstance(typeof(Int32), 10);
+        Array ClientHandles = Array.CreateInstance(typeof(Int32), 10);
+        Array RequestedDataTypes = Array.CreateInstance(typeof(Int16), 10);
+        Array AccessPaths = Array.CreateInstance(typeof(string), 10);
+        Array WriteItems = Array.CreateInstance(typeof(string), 10);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void readData()
+        {
+            List<Resident> list = collection.AsQueryable().ToList();
+
+            dataGridView1.DataSource = list;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readData();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                MyOPCServer.Connect(txtServerName.Text, txtServer.Text);
+                My_OPCGroup = MyOPCServer.OPCGroups.Add("gp1");
+                My_OPCGroup.IsActive = true;
+                My_OPCGroup.IsSubscribed = true;
+                My_OPCGroup.UpdateRate = 1000;    //1 second
+
+                My_OPCGroup.DataChange += new DIOPCGroupEvent_DataChangeEventHandler(My_OPCGroup_DataChange);
+                
+
+
+                ItemCount = 2;
+                OPCItemIDs.SetValue("Channel6.Device1.Simulation.Counter", 1);
+                ClientHandles.SetValue(1, 1);
+
+
+                OPCItemIDs.SetValue("Channel6.Device1.Simulation.Random", 2);
+                ClientHandles.SetValue(2, 2);
+
+                My_OPCGroup.OPCItems.DefaultIsActive = true;
+
+                My_OPCGroup.OPCItems.AddItems(ItemCount, ref OPCItemIDs, ref ClientHandles, out ItemServerHandles, out ItemServerErrors, RequestedDataTypes, AccessPaths);
+
+                timer1.Enabled = !timer1.Enabled;
+                timer2.Enabled = !timer2.Enabled;
+                btnConnect.Enabled = false;
+                btnDisconnect.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Connect! \r\n" + ex.Message);
+            }
+        }
+
+        private void My_OPCGroup_DataChange(int TransactionID, int NumItems, ref Array ClientHandles, ref Array ItemValues, ref Array Qualities, ref Array TimeStamps)
+        {
+            try
+            {
+                for (int i = 1; i <= NumItems; i++)
+                {
+                    if ((Convert.ToInt32(ClientHandles.GetValue(i)) == 1))
+                    {
+                        txtReadVal.Text = ItemValues.GetValue(i).ToString();
+                    }
+                    if ((Convert.ToInt32(ClientHandles.GetValue(i)) == 2))
+                    {
+                        txtWriteVal.Text = ItemValues.GetValue(i).ToString();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            My_OPCGroup.IsSubscribed = false;
+            My_OPCGroup.IsActive = false;
+            MyOPCServer.OPCGroups.RemoveAll();
+            btnDisconnect.Enabled = false;
+            btnConnect.Enabled = true;
+            timer1.Enabled = !timer1.Enabled;
+            timer2.Enabled = !timer2.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string formattedDateTime = now.ToString("dd/MM/yyyy hh:mm:ss tt");
+            Resident resident = new Resident(formattedDateTime, "Int1", txtReadVal.Text);
+
+            collection.InsertOne(resident);
+            readData();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            string formattedDateTime = now.ToString("dd/MM/yyyy hh:mm:ss tt");
+            Resident resident = new Resident(formattedDateTime, "Int2", txtWriteVal.Text);
+
+            collection.InsertOne(resident);
+            readData();
+        }
+    }
+
+    public class Resident
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+
+
+        [BsonElement("Log")]
+        public string Log { get; set; }
+
+        [BsonElement("Tag")]
+        public string Tag { get; set; }
+
+        [BsonElement("Values")]
+        public string Values { get; set; }
+
+        
+
+
+
+        public Resident ( string log, string tag, string value)
+        {
+            Log = log;
+            Tag = tag;
+            Values = value;
+
+        }
+        
+    }
+}
